44f841f8943a50a0cfda18ca98aa0f54c7c88ce0
D3oxys <gilbertoclebe26@gmail.com>
2026-05-07 19:06:38 -0300

    feat(database): Database with sqlite.

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,17 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import create_engine 
 
 
 class Base(DeclarativeBase) :
     pass
+
+DATABASE_URL = "sqlite:///./trackly.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_bd() :
+    db = SessionLocal()
+    try :
+        yield db
+    finally :
+        db.close()
