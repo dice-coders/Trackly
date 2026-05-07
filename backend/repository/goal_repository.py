c0aaae3eb197b3c9dd89e197156656a5a9d42917
D3oxys <gilbertoclebe26@gmail.com>
2026-05-07 19:39:26 -0300

    chore(backend): __init__.py's

---
+++ backend/repository/goal_repository.py
@@ -0,0 +1,15 @@
+
+from sqlalchemy import delete
+import database
+import models.goal_model as model
+
+
+
+def get_goal_by_id(id: int, db = database.get_bd) :
+    return db.get(model.Goal, id)
+
+def delete_goal(id: int, db = database.get_bd) :
+    db.execute(delete(model.Goal).where(model.Goal.id == id))
+    
+    
+    
