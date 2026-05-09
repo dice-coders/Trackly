6a05d70135e70cef273c1ae00512449b1099b7bf
D3oxys <gilbertoclebe26@gmail.com>
2026-05-09 15:20:33 -0300

    fix(repository): goal model import.

--- backend/repository/goal_repository.py
+++ backend/repository/goal_repository.py
@@ -1,21 +1,21 @@
 
 from sqlalchemy import delete, select
 from database import get_db
-import models.goal_model as model
+from models.goal_model import Goal
 
-def create_goal(goal: model.Goal, db = get_db):
+def create_goal(goal: Goal, db = get_db):
     db.add(goal)
     db.commit()
     db.refresh(goal)
     return goal
 
 def get_goal_by_id(id: int, db = get_db) :
-    return db.get(model.Goal, id)
+    return db.get(Goal, id)
 
 def get_all_goals(db = get_db) :
-    return db.scalars(select(model.Goal)).all()
+    return db.scalars(select(Goal)).all()
 
-def update_goal(goal: model.Goal, db = get_db):
+def update_goal(goal: Goal, db = get_db):
     db.add(goal)
     db.commit()
     db.refresh(goal)
@@ -23,7 +23,7 @@ def update_goal(goal: model.Goal, db = get_db):
     return goal
 
 def delete_goal(id: int, db = get_db) :
-    db.execute(delete(model.Goal).where(model.Goal.id == id))
+    db.execute(delete(Goal).where(Goal.id == id))
     db.commit()
     
     
