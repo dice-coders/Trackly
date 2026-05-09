f1cce7f5e49311566b93a304d01a064d4c31fc23
D3oxys <gilbertoclebe26@gmail.com>
2026-05-09 15:19:01 -0300

    fix(repository): database parameter.

--- backend/repository/goal_repository.py
+++ backend/repository/goal_repository.py
@@ -1,28 +1,28 @@
 
 from sqlalchemy import delete, select
-import database
+from database import get_db
 import models.goal_model as model
 
-def create_goal(goal: model.Goal, db = database.get_db):
+def create_goal(goal: model.Goal, db = get_db):
     db.add(goal)
     db.commit()
     db.refresh(goal)
     return goal
 
-def get_goal_by_id(id: int, db = database.get_bd) :
+def get_goal_by_id(id: int, db = get_db) :
     return db.get(model.Goal, id)
 
-def get_all_goals(db = database.get_db) :
+def get_all_goals(db = get_db) :
     return db.scalars(select(model.Goal)).all()
 
-def update_goal(goal: model.Goal, db = database.get_bd):
+def update_goal(goal: model.Goal, db = get_db):
     db.add(goal)
     db.commit()
     db.refresh(goal)
 
     return goal
 
-def delete_goal(id: int, db = database.get_bd) :
+def delete_goal(id: int, db = get_db) :
     db.execute(delete(model.Goal).where(model.Goal.id == id))
     db.commit()
     
