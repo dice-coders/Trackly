00cb18106e774744767f4d5f4412a19eb7a19308
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-05-11 13:33:56 -0300

    feat(schema): User schemas develop.

---
+++ backend/schemas/user_schema.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+from typing import Optional, Literal
+
+class UserBase(BaseModel) :
+    name: str = Field(max_length=120)
+    email: EmailStr = Field(max_length=120)
+    number: str = Field(min_length=11, max_length=11)
+    addres: str
+    role: Literal["ADM"]
+
+class UserCreate(UserBase) :
+    password: str
+    model_config = ConfigDict(from_attributes=True)
+    
+class UserResponse(UserBase) :    
+    model_config = ConfigDict(from_attributes=True)
+    
+class UserUpdate(BaseModel) :
+    name: Optional[str] = Field(default=None, max_length=120)
+    email: Optional[EmailStr] = Field(default=None, max_length=120)
+    number: Optional[str] = Field(default=None, min_length=11, max_length=11)
+    address: Optional[str] = Field(default=None)
+    role: Optional[Literal["ADM"]] = Field(default=None)
+    password: Optional[str] = Field(default=None)
+    
+    model_config = ConfigDict(from_attributes=True)
