0d2020430d566ad43afcb89a10b9a87fb3c906b0
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-05-14 13:38:17 -0300

    feat(model): chat history modelation.

---
+++ backend/models/chat_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Text
+from database import Base
+from datetime import date
+class ChatHistory(Base) :
+    __tablename__ = "chat_history"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    prompt: Mapped[str] = mapped_column(Text)
+    message: Mapped[str] = mapped_column(Text)
+    date_requisition: Mapped[date] = mapped_column(default=date.now)
